d4ac9bd0a35b322f4cfa1c817dccfe014bf71271
mikk5394 <31307509+mikk5394@users.noreply.github.com>
2019-12-27 16:55:23 +0100

    user restrictions

--- auctions/urls.py
+++ auctions/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/auctions') ),
     path('auctions/', views.home, name='auctions-home'),
     path('about/', views.about, name='about-us'),
 ]
--- auctions/views.py
+++ auctions/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Auction
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def home(request):
     context = {
         'auctions': Auction.objects.all()
@@ -9,5 +11,13 @@ def home(request):
     return render(request, 'auctions/home.html', context)
 
 
+@login_required
 def about(request):
     return render(request, 'auctions/about.html')
+
+'''
+@login_required
+def getAuctions(request):
+    
+
+'''
